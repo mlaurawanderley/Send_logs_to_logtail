ccdb5e44a287d6b86a3497eac387e320048979e7
mlaurawanderley <mlaurawferreira@gmail.com>
2022-12-19 09:19:46 -0300

    add files

---
+++ logger.js
@@ -0,0 +1,28 @@
+const { createLogger, transports, format } = require('winston');
+const { Logtail } = require('@logtail/node');
+const { LogtailTransport } = require('@logtail/winston');
+
+
+const logger = createLogger({
+    transports: [
+        new transports.Console(),
+        new transports.File({ 
+            level: 'warn',
+            filename: 'logsWarning.log'
+         }),
+         new transports.File({ 
+            level: 'error',
+            filename: 'logsError.log',
+            handleExceptions: true
+            }),
+        new LogtailTransport(new Logtail(<'Seu sourceToken aqui'>))
+    ],
+    format: format.combine(
+        format.timestamp(),
+        format.json(),
+        format.prettyPrint()
+    ),
+});
+
+module.exports = logger;
+
